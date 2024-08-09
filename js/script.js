fcbc7ad525060fb46d23726828cdb28f30c8b9b5
Fernando Contreras <fcontreras.dev@gmail.com>
2024-08-09 19:39:00 -0400

    agregando validacion expresion regular y correo

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contact-form');
-  
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-  
-      const nombre = document.getElementById('nombre').value;
-      const email = document.getElementById('email').value;
-      const mensaje = document.getElementById('mensaje').value;
-  
-      if (nombre && email && mensaje) {
-        alert(`¡Gracias, ${nombre}! Tu mensaje ha sido enviado`);
-        form.reset(); // Limpiar formulario
-      } else {
-        alert('Debe completar todos los campos para de enviar');
-      }
-    });
+  const form = document.getElementById('contact-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    // Verificar que el nombre solo contenga letras de la a a la z (mayúsculas y minúsculas)
+    const nombreRegex = /^[a-zA-Z]+$/;
+
+    if (!nombreRegex.test(nombre)) {
+      alert('El nombre solo debe contener letras de la A a la Z');
+      return;
+    }
+
+    // Verificar que el email tenga un formato correcto
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      alert('Por favor, ingresa un email válido');
+      return;
+    }
+
+    if (nombre && email && mensaje) {
+      alert(`¡Gracias, ${nombre}! Tu mensaje ha sido enviado`);
+      form.reset(); // Limpiar formulario
+    } else {
+      alert('Debe completar todos los campos para enviar el mensaje');
+    }
   });
-  
+});
